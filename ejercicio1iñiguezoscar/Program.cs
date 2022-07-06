6a49386ab7c5c9a5b3537a586f80e6bd632df289
Usuario <Usuario@DESKTOP-STUV9M6>
2022-07-05 22:58:37 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio1iñiguezoscar/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace PSeInt
+{
+	class ejercio01iniguezoscar
+	{
+
+		static void Main(string[] args)
+		{
+			int dolarcompra;
+			double eurocompra;
+			int pesos;
+			// ingresar las variables
+			Console.Write(" ingrese la cantidad de pesos que tiene:");
+			pesos = int.Parse(Console.ReadLine());
+			// Proceso 
+			dolarcompra = pesos * 230;
+			eurocompra = dolarcompra * 1.17;
+			// ingreso de informacion
+			Console.WriteLine(" podemos comprar con pesos esta cantidad de doares:" + dolarcompra);
+			Console.WriteLine("podes comprar con pesos esta cantidad de euros:" + eurocompra);
+		}
+
+	}
+
+}
+
